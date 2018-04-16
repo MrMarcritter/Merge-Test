19bbc20ee5cf2c5921363327ab6493457a8ef992
Marco <Communication@live.se>
2018-04-16 02:44:37 +0200

    Added Marco.xaml
    Added Marco.xaml along with base similar to Timmy.xaml and Christer.xaml
    *Known issue - Page not working.

---
+++ MergeTestGrund/MergeTestGrund/MergeTestGrund/Marco.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MergeTestGrund
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Marco : ContentPage
+	{
+		public Marco ()
+		{
+			InitializeComponent ();
+		}
+
+
+        private async void Button_OnClicked_C(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Christer());
+        }
+
+        private async void Button_OnClicked_T(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Timmy());
+        }
+    }
+}
