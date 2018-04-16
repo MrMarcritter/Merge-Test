52b80d1f4894daba7a86840d1c5e9baa38b2bf2d
tstaaf <timmystaaf@gmail.com>
2018-04-16 10:50:24 +0200

    meh

---
+++ GridTest/GridTest/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GridTest
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+	    private async void TapGestureRecognizer_OnTapped(object sender, EventArgs e)
+	    {
+	        await Navigation.PushAsync(new Page1());
+	    }
+
+	    private void TapGestureRecognizer_OnTapped1(object sender, EventArgs e)
+	    {
+	        DisplayAlert("Tapped Onboarding", "You tapped Onboarding", "OK");
+        }
+
+	    private void TapGestureRecognizer_OnTapped2(object sender, EventArgs e)
+	    {
+	        DisplayAlert("Tapped Schedule", "You tapped Schedule", "OK");
+	    }
+	}
+}
